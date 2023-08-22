9e5abff93eac5f15e002556b47953e74bff03676
admin@knitpay.org <admin@knitpay.org>
2023-08-22 21:19:45 +0530

    WooCommerce: Temporarily hiding Payment Button Label.

--- src/PaymentMethodType.php
+++ src/PaymentMethodType.php
@@ -150,7 +150,7 @@ class PaymentMethodType extends AbstractPaymentMethodType {
 			'description'      => $description,
 			'fields'           => $gateway->get_input_fields(),
 			'icon'             => $this->get_setting( 'icon' ),
-			'orderButtonLabel' => $order_button_label,
+			// 'orderButtonLabel' => $order_button_label, // Temporarily commented by Knit Pay.
 			'supports'         => $this->get_supported_features(),
 		];
 	}
