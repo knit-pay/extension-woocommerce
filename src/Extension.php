eebc9c3da8073ae84562cac06adbe297d724a78c
admin@knitpay.org <admin@knitpay.org>
2023-11-17 17:47:14 +0530

    Renamed Pronamic Pay to Knit Pay to avoid confusion.

--- src/Extension.php
+++ src/Extension.php
@@ -1379,7 +1379,7 @@ class Extension extends AbstractPluginIntegration {
 
 		\add_meta_box(
 			'woocommerce-order-pronamic-pay',
-			\__( 'Pronamic Pay', 'pronamic_ideal' ),
+			\__( 'Knit Pay', 'pronamic_ideal' ),
 			function () use ( $order ) {
 				include __DIR__ . '/../views/admin-meta-box-woocommerce-order.php';
 			},
